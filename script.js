46ba824abf8b90f214b2dd3e8330c5c2440163df
Void <143309999+VVoiddd@users.noreply.github.com>
2023-09-30 07:35:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to update the clock
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    document.getElementById('clock').innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
