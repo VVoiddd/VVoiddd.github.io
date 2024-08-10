4e45e024d112345fd6e4eb514d13e406144859f3
VVoiddd <143309999+VVoiddd@users.noreply.github.com>
2024-08-10 15:36:33 -0400

    Moar Toolz

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const tools = document.querySelectorAll('.tool');
-
-    // Apply the 'show' class to each tool to trigger the fade-in animation
     tools.forEach((tool, index) => {
         setTimeout(() => {
             tool.classList.add('show');
-        }, index * 200); // Stagger animation for a smoother effect
+        }, index * 200);
     });
 });
